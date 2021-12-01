466724b63c4a45a99d39e23b9792fe73b3802d8b
zagitullin_ii.sitgit config --global user.name zagitullin_ii.sitgit config --global user.name zagitullin_ii.sit <mup_@inbox.ru>
2021-12-01 13:15:02 +0300

    new commit

---
+++ IdeaProjects/Proekt demo/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        int Balans = 2_000_000_000;
+        int replenishment = 5_000_000;
+        int total = Balans + replenishment;
+        System.out.println(total);
+    }
+}
